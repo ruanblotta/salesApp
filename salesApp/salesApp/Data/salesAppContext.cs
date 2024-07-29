0911d60181b97c90044a78e2bc9f8430aa93bfe5
Ruan Blotta <ruanblotta@gmail.com>
2024-07-29 17:37:40 -0300

    First Commit

---
+++ salesApp/salesApp/Data/salesAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using salesApp.Models;
+
+namespace salesApp.Data
+{
+    public class salesAppContext : DbContext
+    {
+        public salesAppContext (DbContextOptions<salesAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<salesApp.Models.Department> Department { get; set; } = default!;
+    }
+}
---
+++ salesApp/salesApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace salesApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
